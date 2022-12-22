9aed9b887b55c294051fa809e6c9029e13423f45
Paul Reiter <reiterpaul02@gmail.com>
2022-12-22 10:13:45 +0100

    Add: Variablen + Comments

--- snake.js
+++ snake.js
@@ -1,6 +1,7 @@
 // Snake
 // Spielfunktion
 
+// Variablen: -----------------------------------
 // Variablen um auf HTML Datei zuzugreifen
 let canvas = document.getElementById('canvas');
 let ctx = canvas.getContext('2d');
@@ -8,6 +9,22 @@ let ctx = canvas.getContext('2d');
 // Legt die Größe der Kästchen für die Spiel-
 // figur fest
 let rectsize = 30 - 1;
+
+// Variablem für den Spiellauf
+let rows = 24;
+let cols = 14;
+
+// Koordinaten Schlange + Essen
+let snake;
+let food;
+
+// Zellengröße
+let cellWidth = canvas.width/cols;
+let cellHeight = canvas.height/rows;
+
+// Richtung, in die die Schlange zeigt
+let direction = 'LEFT';
+// ----------------------------------------------
 
 // Führt "draw()" aus
 draw();
@@ -20,13 +37,22 @@ function draw() {
 
     // Spielfigut/Schlange
     ctx.fillStyle = 'lightgreen';
-    ctx.fillRect(100, 150, rectsize, rectsize);
-
-    ctx.fillRect(130, 150, rectsize, rectsize);
+    add(100, 150);
+    add(130, 150);
+    add(160, 150);
+    add(190, 150);
+    add(190, 180);
+    add(190, 210);
+    add(220, 210);
 
     // Äpfel/Essen
     ctx.fillStyle = 'red';
-    ctx.fillRect(290, 250, rectsize, rectsize);
+    add(290, 250);
+}
+
+// Legt das Kästchen fest
+function add(x, y){
+    ctx.fillRect(x, y, rectsize, rectsize);
 }
 
 // Spielfunktion
