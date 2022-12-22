8c5a2a6d734bc7ce0d2ccdb1e0b73cec4f5e2fbe
Paul Reiter <reiterpaul02@gmail.com>
2022-12-22 08:52:38 +0100

    Add: Erste Blöcke

--- snake.js
+++ snake.js
@@ -4,12 +4,27 @@
 let canvas = document.getElementById('canvas');
 let ctx = canvas.getContext('2d');
 
-// Optik
-function draw(){
+// Legt die Größe der Kästchen für die Spiel-
+// figur fest
+let rectsize = 30 - 1;
 
+draw();
+
+// Optik
+function draw() {
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    ctx.fillStyle = 'lightgreen';
+    ctx.fillRect(100, 150, rectsize, rectsize);
+
+    ctx.fillRect(130, 150, rectsize, rectsize);
+
+    ctx.fillStyle = 'red';
+    ctx.fillRect(290, 250, rectsize, rectsize);
 }
 
 // Spielfunktion
-function gameLoop(){
+function gameLoop() {
 
 }
